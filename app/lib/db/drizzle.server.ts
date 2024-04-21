50a46c15dc98df97ee3376c8f9040594d1dfe536
VaggelisKa <e.karavasileiadis@gmail.com>
2024-04-21 20:02:10 +0200

    Add log

--- app/lib/db/drizzle.server.ts
+++ app/lib/db/drizzle.server.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
 if (!process.env.DATABASE_URL) {
+  console.log(process.env.DATABASE_URL);
   throw new Error("DATABASE_URL is not set");
 }
 
