7b228b11beb532e52ccc5d7b5d96819cfe4b6a23
VaggelisKa <e.karavasileiadis@gmail.com>
2024-04-21 20:16:41 +0200

    Downgrade libsql client

--- app/lib/db/drizzle.server.ts
+++ app/lib/db/drizzle.server.ts
@@ -4,14 +4,7 @@ import { createClient } from "@libsql/client";
 
 dotenv.config();
 
-console.log(
-  "credentials => ",
-  process.env.DATABASE_URL,
-  process.env.DATABASE_AUTH_TOKEN,
-);
-
 if (!process.env.DATABASE_URL) {
-  console.log(process.env.DATABASE_URL);
   throw new Error("DATABASE_URL is not set");
 }
 
