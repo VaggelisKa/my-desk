96f70ceb8b6e97f9b30e52ac2bf17a47841a4cf0
VaggelisKa <e.karavasileiadis@gmail.com>
2024-04-27 08:44:29 +0200

    Querying reservations

--- app/routes/reservations.tsx
+++ app/routes/reservations.tsx
@@ -1,5 +1,35 @@
+import { useLoaderData } from "@remix-run/react";
+import { type LoaderFunctionArgs } from "@vercel/remix";
+import { eq } from "drizzle-orm";
 import { ReservationsTable } from "~/components/reservations-table";
+import { requireAuthCookie } from "~/cookies.server";
+import { db } from "~/lib/db/drizzle.server";
+import { reservations } from "~/lib/db/schema.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  let userId = await requireAuthCookie(request);
+  let reservationsRes = await db.query.reservations.findMany({
+    with: {
+      desks: {
+        columns: {
+          id: false,
+          userId: false,
+        },
+      },
+    },
+    columns: {
+      userId: false,
+    },
+    where: eq(reservations.userId, userId),
+  });
+
+  console.log(reservationsRes);
+
+  return { reservations: reservationsRes };
+}
 
 export default function ReservationsPage() {
+  let { reservations } = useLoaderData<typeof loader>();
+
   return <ReservationsTable />;
 }
