7a0d76ab8086067bb2b36943073a0fb318ac031a
VaggelisKa <e.karavasileiadis@gmail.com>
2024-05-13 20:34:16 +0200

    Starting work on filters

---
+++ app/components/filters-form.tsx
@@ -0,0 +1,52 @@
+import { Form, useSubmit } from "@remix-run/react";
+import { useRef } from "react";
+import { Checkbox } from "~/components/ui/checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+
+export function FiltersForm() {
+  let formRef = useRef<HTMLFormElement | null>(null);
+  let submit = useSubmit();
+
+  return (
+    <Form ref={formRef} method="GET" className="flex flex-col gap-8">
+      <fieldset className="flex items-center gap-1">
+        <Checkbox
+          id="terms"
+          onCheckedChange={() => submit(formRef.current)}
+          name="show-free"
+        />
+        <label
+          htmlFor="terms"
+          className="text-sm font-medium  peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Show free desks only
+        </label>
+      </fieldset>
+
+      <fieldset className="space-y-2">
+        <p className="text-sm font-medium capitalize leading-none">
+          Desk placement
+        </p>
+        <Select name="column" onValueChange={() => submit(formRef.current)}>
+          <SelectTrigger className="w-full sm:w-[180px]">
+            <SelectValue placeholder="Placement of desk" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="1">window</SelectItem>
+              <SelectItem value="2">middle</SelectItem>
+              <SelectItem value="3">isle</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </fieldset>
+    </Form>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunctionArgs, MetaFunction } from "@vercel/remix";
 import { DeskButton } from "~/components/desk-button";
 import { DeskModal } from "~/components/desk-selection-modal";
+import { FiltersForm } from "~/components/filters-form";
 import { requireAuthCookie } from "~/cookies.server";
 import { db } from "~/lib/db/drizzle.server";
 
@@ -11,8 +12,11 @@ export const meta: MetaFunction = () => {
 
 export async function loader({ request }: LoaderFunctionArgs) {
   let { userId } = await requireAuthCookie(request);
+  let url = new URL(request.url);
+  let showFree = url.searchParams.get("show-free");
+  let column = url.searchParams.get("column");
 
-  let desks = await db.query.desks.findMany({
+  let desksRes = await db.query.desks.findMany({
     columns: {
       block: true,
       row: true,
@@ -27,10 +31,19 @@ export async function loader({ request }: LoaderFunctionArgs) {
       },
       user: true,
     },
+    where: (desks, { eq }) =>
+      Number(column) ? eq(desks.column, Number(column)) : undefined,
   });
 
-  let desksAggregatedByBlock = desks.reduce(
+  let desksAggregatedByBlock = desksRes.reduce(
     (acc, desk) => {
+      if (
+        showFree &&
+        !!desk.reservations.find((r) => r.date === "14.05.2024")
+      ) {
+        return acc;
+      }
+
       if (!acc[desk.block]) {
         acc[desk.block] = [];
       }
@@ -39,7 +52,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
       return acc;
     },
-    {} as Record<number, typeof desks>,
+    {} as Record<number, typeof desksRes>,
   );
 
   return { desks: desksAggregatedByBlock, userId };
@@ -49,14 +62,16 @@ export default function Index() {
   let data = useLoaderData<typeof loader>();
 
   return (
-    <section>
-      <div className="flex flex-col gap-8">
-        {Object.entries(data.desks).map(([block, desks]) => {
+    <section className="flex flex-col gap-16 md:flex-row md:gap-24">
+      <FiltersForm />
+
+      <div className="flex flex-col gap-8 flex-1">
+        {Object.entries(data.desks).map(([block, desksData]) => {
           return (
             <div key={block} className="flex flex-col gap-2">
               <span className="text-lg font-bold">Block {block}</span>
               <div className="grid grid-cols-3 gap-2">
-                {desks.map((desk) => (
+                {desksData.map((desk) => (
                   <DeskModal
                     key={desk.id}
                     TriggerElement={<DeskButton name={desk.user?.firstName} />}
