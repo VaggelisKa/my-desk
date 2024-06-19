c8cb8d1bc012b3615ee3a3941e948c812c9ce45d
VaggelisKa <e.karavasileiadis@gmail.com>
2024-06-19 21:08:18 +0200

    Add addCron method

---
+++ app/lib/easy-cron.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+const BASE_URL = "https://www.easycron.com/rest";
+const GROUP_ID = "39667";
+
+export const addCronSchema = z.object({
+  days: z.array(
+    z.enum(["monday", "tuesday", "wednesday", "thursday", "friday"]),
+  ),
+  deskId: z.string(),
+  userId: z.string(),
+  firstName: z.string(),
+  lastName: z.string().optional(),
+});
+
+export async function addCron({
+  deskId,
+  days,
+  userId,
+  firstName,
+  lastName,
+}: z.infer<typeof addCronSchema>): Promise<{
+  status: string;
+  cron_job_id: string;
+}> {
+  let url = new URL(`${BASE_URL}/add`);
+  let callbackUrl = new URL(
+    "https://share-a-desk.vercel.app/cron/automatic-reservation",
+  );
+  callbackUrl.searchParams.set("deskId", deskId);
+  callbackUrl.searchParams.set("userId", userId);
+  days.forEach((day) => callbackUrl.searchParams.append("day", day));
+
+  url.searchParams.set("token", process.env.CRON_TOKEN!);
+  url.searchParams.set("url", callbackUrl.toString());
+  url.searchParams.set("cron_expression", "0 10 * * 7");
+  url.searchParams.set(
+    "cron_job_name",
+    `auto-reservations-for-${firstName.toLowerCase()}-${lastName?.toLowerCase()}`,
+  );
+  url.searchParams.set("group_id", GROUP_ID);
+
+  let response = await fetch(url.toString(), { method: "POST" });
+  let json = await response.json();
+
+  return json;
+}
--- app/routes/automatic-reservations.tsx
+++ app/routes/automatic-reservations.tsx
@@ -1,5 +1,9 @@
 import { Form, useLoaderData } from "@remix-run/react";
-import { redirect, type LoaderFunctionArgs } from "@vercel/remix";
+import {
+  redirect,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+} from "@vercel/remix";
 import { eq } from "drizzle-orm";
 import { Button } from "~/components/ui/button";
 import { Checkbox } from "~/components/ui/checkbox";
@@ -7,6 +11,7 @@ import { TypographyH1 } from "~/components/ui/typography";
 import { requireAuthCookie } from "~/cookies.server";
 import { db } from "~/lib/db/drizzle.server";
 import { desks } from "~/lib/db/schema.server";
+import { addCron, addCronSchema } from "~/lib/easy-cron";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   let user = await requireAuthCookie(request);
@@ -24,6 +29,34 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return { desk };
 }
 
+export async function action({ request }: ActionFunctionArgs) {
+  let user = await requireAuthCookie(request);
+  let formData = await request.formData();
+  let days = formData.getAll("day");
+  let deskId = String(formData.get("deskId"));
+
+  let parsedInput = addCronSchema.safeParse({
+    days,
+    deskId,
+    userId: user.userId,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  });
+
+  if (!parsedInput.success) {
+    return null;
+  }
+
+  try {
+    let res = await addCron(parsedInput.data);
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return null;
+}
+
 export default function AutomaticReservationsPage() {
   let data = useLoaderData<typeof loader>();
   let availableDays = ["monday", "tuesday", "wednesday", "thursday", "friday"];
@@ -38,7 +71,7 @@ export default function AutomaticReservationsPage() {
         <fieldset className="flex flex-wrap items-center gap-6">
           {availableDays.map((day) => (
             <div key={day} className="flex items-center space-x-2">
-              <Checkbox id={`day-${day}`} name={day} />
+              <Checkbox id={`day-${day}`} name="day" value={day} />
               <label
                 htmlFor={`day-${day}`}
                 className="text-sm font-medium capitalize leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
