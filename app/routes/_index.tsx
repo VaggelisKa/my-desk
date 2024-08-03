fb20bc0c11f1c56279d38be364b4d41f033d75fd
VaggelisKa <e.karavasileiadis@gmail.com>
2024-08-03 08:40:20 +0200

    test 3

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ import { DeskModal } from "~/components/desk-selection-modal";
 import { FiltersForm } from "~/components/filters-form";
 import { requireAuthCookie } from "~/cookies.server";
 import { db } from "~/lib/db/drizzle.server";
-import { desks, reservations } from "~/lib/db/schema.server";
+import { desks } from "~/lib/db/schema.server";
 import { cn } from "~/lib/utils";
 
 export const meta: MetaFunction = () => {
@@ -40,7 +40,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
   // });
 
   await db.select().from(desks);
-  await db.select().from(reservations);
 
   let desksRes: any = {
     desks: {
